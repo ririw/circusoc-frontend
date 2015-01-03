53fde19d54f72b9d6f1c2eeec909dc49964e86c1
Richard Weiss <richardweiss@richardweiss.org>
2015-01-03 18:00:17 +0300

    Cleanups

--- circus-main/circus-main.js
+++
@@ -1,9 +0,0 @@
-/*app.directive('circus-main', function () {
-    return {
-        restrict:'EA',
-        transclude:false,
-        scope:{},
-        templateUrl: "circus-main/circus-main.html",
-        replace:true
-    };
-});*/
