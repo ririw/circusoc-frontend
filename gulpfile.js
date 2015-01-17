0fdf38000fabc51483d434b4c7c7ca4ef6d70fd4
Richard Weiss <richardweiss@richardweiss.org>
2015-01-17 11:12:56 +0100

    more or less working build

--- gulpfile.js
+++ gulpfile.js
@@ -36,16 +36,113 @@ var gulp = require('gulp'),
         "circus-join/circus-join.css",
         "circus-login/circus-login.css"
     ],
+    html_files = [
+        'index.max.html',
+        'circus-artists/circus-artists.html',
+        'circus-contact/circus-contact.html',
+        'circus-hire/circus-hire.html',
+        'circus-join/circus-join.html',
+        'circus-login/circus-login.html',
+        'circus-main/circus-main.html',
+        'performer/performer.html'
+    ],
     devmode = false;
 
+
+
+var js_compressor = closureCompiler({
+        compilerPath: 'bower_components/closure-compiler/compiler.jar',
+        fileName: 'intermediate.js',
+        compilerFlags: {
+            compilation_level: 'ADVANCED_OPTIMIZATIONS'
+        }
+    });
+
+
+// JS COMPRESSION
+if (devmode)
+    gulp.task('compress-js', function() {
+        gulp.src(minified_files)
+            .pipe(gulp.dest('build/'))
+    });
+else
+    gulp.task('compress-js', function() {
+        gulp.src(minified_files)
+            .pipe(js_compressor)
+            .pipe(gulp.dest('build/'))
+    });
+
+// JS MERGE
+if (devmode)
+    gulp.task('merge-js', ['compress-js'], function() {
+        gulp.src(concat_files)
+            .pipe(gulp.dest('build/'))
+    });
+else
+    gulp.task('merge-js', ['compress-js'], function() {
+        gulp.src(concat_files)
+            .pipe(concat('all.min.js'))
+            .pipe(gulp.dest('build/'))
+    });
+
+
+// CSS COMPRESSION
+if (devmode)
+    gulp.task('compress-css', [], function() {
+        gulp.src(css_files)
+            .pipe(gulp.dest('build/'))
+    });
+else
+    gulp.task('compress-css', function() {
+        gulp.src(css_files)
+            .pipe(compressor())
+            .pipe(gulp.dest('build/'))
+    });
+
+// HTML INLINING & MINIFICATION
+if (devmode)
+    gulp.task('inline-html', ['merge-js', 'compress-css'], function() {
+        gulp.src(html_files)
+            .pipe(gulp.dest('build/'));
+    });
+else
+    gulp.task('inline-html', ['merge-js', 'compress-css'], function() {
+        gulp.src(html_files)
+            .pipe(inlinesource())
+            .pipe(minifyHTML({empty: true}))
+            .pipe(gulp.dest('build/'));
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if (devmode) {
-    gulp.task('compress', function () {
+    gulp.task('compress', [], function () {
         gulp.src(minified_files)
             .pipe(concat('intermediate.js'))
             .pipe(gulp.dest('build/'))
     });
-    gulp.task('css-minify', function() {
+
+    gulp.task('css-minify', [], function() {
         gulp.src(css_files)
+            .pipe(gulp.dest('build/'))
+    });
+
+    gulp.task('concat', ['compress'], function() {
+        gulp.src(concat_files)
+            .pipe(concat('all.min.js'))
             .pipe(gulp.dest('build/'))
     });
 
@@ -59,10 +156,14 @@ if (devmode) {
         gulp.src(minified_files)
             .pipe(closureCompiler({
                 compilerPath: 'bower_components/closure-compiler/compiler.jar',
-                fileName: 'intermediate.js'
+                fileName: 'intermediate.js',
+                compilerFlags: {
+                    compilation_level: 'ADVANCED_OPTIMIZATIONS'
+                }
             }))
             .pipe(gulp.dest('build/'));
     });
+
 
     gulp.task('css-minify', function() {
         gulp.src(css_files)
@@ -70,11 +171,11 @@ if (devmode) {
             .pipe(gulp.dest('build/'))
     });
 
-    gulp.task('inline', ['concat', 'css-minify'], function() {
+    gulp.task('inline', ['compress', 'concat', 'css-minify'], function() {
         gulp.src('index.max.html')
             .pipe(rename('index.html'))
             .pipe(inlinesource())
-            .pipe(minifyHTML({comments:true,spare:true}))
+            .pipe(minifyHTML({empty: true}))
             .pipe(gulp.dest('.'));
     });
 }
@@ -86,7 +187,6 @@ gulp.task('concat', ['compress'], function() {
         .pipe(gulp.dest('build/'))
 });
 
-
 gulp.task('watch', function(){
    gulp.watch(minified_files, ['compress']);
    gulp.watch('index.max.html', ['inline']);
@@ -95,4 +195,4 @@ gulp.task('watch', function(){
 });
 
 gulp.task('default', ['inline']);
-//gulp.task('default', ['concat', 'compress', 'css-minify', 'watch']);
+// gulp.task('default', ['inline', 'watch']);
